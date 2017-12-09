a8cbf1d10f5f1e6a386632a61fca066b4435f216
Claudio Bantaloukas <rockdreamer@gmail.com>
2017-12-09 10:11:24 +0000

    use boost license

--- tests/shared_ptr_dereference.cpp
+++ tests/shared_ptr_dereference.cpp
@@ -1,3 +1,8 @@
+//          Copyright Claudio Bantaloukas 2017.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
 #include <gtest/gtest.h>
 #include <throwing/shared_ptr.hpp>
 
