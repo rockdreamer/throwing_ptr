94f336faf4017c3cb390eb2131f6e74741f5859b
Claudio Bantaloukas <rockdreamer@gmail.com>
2018-02-26 19:14:05 +0000

    re-add arch to conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -10,7 +10,7 @@ class ThrowingPtrConan(ConanFile):
     url = "https://github.com/rockdreamer/throwing_ptr"
     description = "shared_ptr that throws on dereference if null"
     exports_sources = "*"
-    settings = "os", "compiler"
+    settings = "os", "compiler", "arch"
     generators = "cmake"
 
     build_requires = "catch2/2.1.2@bincrafters/stable"
