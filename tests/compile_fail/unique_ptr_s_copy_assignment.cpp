b6f75d29d1d7265668d91bf094664be89f2cac25
Claudio Bantaloukas <rockdreamer@gmail.com>
2018-03-08 20:08:46 +0000

    update copyright headers

--- tests/compile_fail/unique_ptr_s_copy_assignment.cpp
+++ tests/compile_fail/unique_ptr_s_copy_assignment.cpp
@@ -1,3 +1,8 @@
+//          Copyright Claudio Bantaloukas 2017-2018.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
 #include <throwing/unique_ptr.hpp>
 
 int main() {
